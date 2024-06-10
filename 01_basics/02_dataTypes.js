2caafdf98f2734d48b991acbf2eed00ef7ea4f99
M.MUDASSIR MUNIR <58942002+Mudassir-Munir@users.noreply.github.com>
2024-06-10 12:51:14 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+
+//alert(3 + 3)  // not valid for node js, // only valid in browser
+
+// data types
+
+// number => 2 raise to power 53
+// bigint
+// string
+// boolean
+// null =>  note: null is standalone value
+// undefinded
+// symbol => unique (to check uniqueness)
+
+// non-primitive
+// object
+
+console.log(typeof null); // it will be object
+console.log(typeof undefined); // it will be undefined
+
+// ECMA script language specification
+// https://tc39.es/ecma262/#sec-intro
+
+// visit mdn documention for js syntax 
+// https://developer.mozilla.org/en-US/docs/Web/Api/Window/alert
